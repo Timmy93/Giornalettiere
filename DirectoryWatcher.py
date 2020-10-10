7f045187bdfbdcf22437c8a4f92ec5f16ae9e955
Timmy93 <federico.seri@mail.polimi.it>
2020-10-10 18:27:41 +0200

    Wait complete file write before uploading

--- DirectoryWatcher.py
+++ DirectoryWatcher.py
@@ -3,6 +3,7 @@ import threading
 import inotify.adapters
 import logging
 import os
+import time
 
 # The main class
 class DirectoryWatcher (threading.Thread):
@@ -22,11 +23,20 @@ class DirectoryWatcher (threading.Thread):
 					if event is not None:
 						# ~ print(event)
 						if 'IN_CREATE' in event[1] or 'IN_MOVED_TO' in event[1]:
-							self.logging.info("Created/moved a file/directory ["+os.path.join(event[2], event[3])+"]")
+							newfile = os.path.join(event[2], event[3])
+							self.logging.info("Created/moved a file/directory ["+newfile+"]")
+							while not self.stableSize(newfile):
+								self.logging.info("Still writing on file ["+newfile+"]")
+								#TODO check that file has been completely written, do not wait 1 seconds and recheck 
 							self.bot.updateChannel()
 				self.logging.warning("DirectoryWatcher stopped checking directory")
 			except RuntimeError:
 				self.logging.warning("Thread is dead for a Runtime error, restarting")
 			else:
 				self.logging.warning("Thread is dead, restarting")
-		
+	
+	#Check if the file is still being written
+	def stableSize(self, myFile):
+		initialSize = os.path.getsize(myFile)
+		time.sleep(1)
+		return os.path.getsize(myFile) == initialSize
